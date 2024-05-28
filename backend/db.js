687be0d5da67cdae78245c6c3626a0f34b18256c
Md-shafi-ur-rehaman <mohammadShafiq.rahaman@gmail.com>
2024-05-28 15:30:03 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    lowercase:true,
+    minLength:3,
+    maxLength:30
+  },
+  password: {
+    type:String,
+    required:true,
+    minLength:6
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50
+  },
+  lastname:{
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50
+  }
+})
+
+const user = mongoose.modle("User",userSchema);
+
+module.export = {
+  User
+}
